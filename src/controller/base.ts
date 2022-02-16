2731ed2141a178213716407ea5b178748349f668
anderson-santos-sidia <anderson.santos@sidia.com>
2022-02-16 14:27:28 -0300

    better desc

--- src/controller/base.ts
+++ src/controller/base.ts
@@ -5,9 +5,9 @@ import { User } from "../dto/user";
 const prisma = new PrismaClient()
 
 export class BaseController {
+    
+    //get info about status of servers or services and return the result
     public async getStatus(req: Request, res: Response){
-        //get info about status of servers or services and return the result
-        // func
         const servicesStatus = {
             ServerA:true,
             ServerB: false,
@@ -18,15 +18,14 @@ export class BaseController {
         return res.json({status:1,message:"",detail:"",date: new Date(),content:servicesStatus})
     }
 
+    //get the request body json to use to send data or orders to other services
     public async setStatus(req: Request, res: Response){
-        //get the request body json to use to send data or orders to other services
         const {status} = req.body;
         return res.json({status,date:new Date()})
     }
 
+    //get a json with all users in the BD
     public async getUsersList(req: Request, res: Response){
-        //get info about status of servers or services and return the result
-        // func
         let allUsers;
         try {
             allUsers = await prisma.user.findMany()
@@ -37,9 +36,8 @@ export class BaseController {
         return res.json({status:1,message:null,detail:null,date: new Date(),content:allUsers})
     }
 
+    //Save a new user on the BD
     public async setNewUser(req: Request<{},{},User>, res: Response){
-        //get info about status of servers or services and return the result
-        // func
         const user = req.body;
         let allUsers;
         try {
